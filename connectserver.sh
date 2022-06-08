56f8c84944f9a07d0fbc5bb3c5eee1379d1a2642
rvcabc <brycejcorbin@gmail.com>
2022-06-08 13:26:09 -0700

    Initial Commit

---
+++ connectserver.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## This program allows easier connection to ssh servers
+## You will need to add servers independently here.
+## This will update the users personal .ssh/config file.
+
+cat welcomemessage
