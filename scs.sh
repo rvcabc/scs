a7d6ef176c481fb8408cfcec66ad6bc21b0681d4
rvcabc <brycejcorbin@gmail.com>
2022-06-08 15:36:41 -0700

    Created bin folder. Added separate files for program.

--- scs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## This program allows easier connection to ssh servers
-## You will need to add servers independently here.
-## This will update the users personal .ssh/config file.
-
-cat bin/welcomemessage
-
